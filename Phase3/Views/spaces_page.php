51ed07011983d6e3c3f0028914ff410fe9d45bd5
iamnsrivastava <35356736+iamnsrivastava@users.noreply.github.com>
2018-04-11 12:20:23 -0400

    Add files via upload

--- Phase3/Views/spaces_page.php
+++ Phase3/Views/spaces_page.php
@@ -1,38 +1,66 @@
 <?php
 
 echo "Welcome to the Space Page <br>";
-$space_name = $_POST[space_name];
-$street = $_POST["street"];
-$apt = $_POST["apt"];
-$state = $_POST["state"];
-$zipcode = $_POST["zipcode"];
-$availability_from = $_POST["from"];
-$availability_till = $_POST["till"];
-$categories = $_POST["Categories"];
-$PricePerHour = $POST["price"];
-$description = $POST["description"];
-$Amenities = $POST["amenities"];
+// $space_name = $_POST["space_name"];
+// $street = $_POST["street"];
+// $apt = $_POST["apt"];
+// $state = $_POST["state"];
+// $zipcode = $_POST["zipcode"];
+// $availability_from = $_POST["from"];
+// $availability_till = $_POST["till"];
+// $categories = $_POST["categories"];
+// $PricePerHour = $_POST["price"];
+// $description = $_POST["description"];
+// $Amenities = $_POST["amenities"];
 // Need to add $Pictures = $POST["pic"];
+
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "database_phase3"
+$dbname = "DatabusSchema";
 
 //Creating Connection
 $conn = mysqli_connect($servername,$username,$password,$dbname);
+$space_name = mysqli_real_escape_string($conn, $_REQUEST['space_name']);
+$street = mysqli_real_escape_string($conn, $_REQUEST['street']);
+$apt = mysqli_real_escape_string($conn, $_REQUEST['apt']);
+$state = mysqli_real_escape_string($conn, $_REQUEST['state']);
+$zipcode = mysqli_real_escape_string($conn, $_REQUEST['zipcode']);
+$availability_from = mysqli_real_escape_string($conn, $_REQUEST['from']);
+$availability_till = mysqli_real_escape_string($conn, $_REQUEST['till']);
+$categories =  mysqli_real_escape_string($conn, $_REQUEST['categories']);
+$PricePerHour = mysqli_real_escape_string($conn, $_REQUEST['price']);
+$description = mysqli_real_escape_string($conn, $_REQUEST['description']);
+$Amenities = mysqli_real_escape_string($conn, $_REQUEST['amenities']);
+// $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
+// $name = mysqli_real_escape_string($conn, $_REQUEST['name']);
+// $pass = mysqli_real_escape_string($conn, $_REQUEST['password']);
+// $phone = mysqli_real_escape_string($conn, $_REQUEST['phone']);
+// $last = mysqli_real_escape_string($conn, $_REQUEST['last']);
+
+
+
 //Checking Connection
 if (!$conn) {
   die("Connection Failed: " .mysqli_connect_error());
 }
-//SQL Statements 
-$sql = "INSERT INTO Space (space_name,resv_start_date,resv_end_date,price,service) VALUES('$_POST["space_name"]','$_POST["from"]','$_POST["till"]'','$_POST["price"]','$_POST[amenities]');";
-$sql1 = "INSERT INTO Space_addr (street,room,state,zip_code,) VALUES('$_POST[".$street"]','$_POST[".$apt"]','$_POST[".$state"]'','$_POST[".$zipcode"]');";
-$sql2 = "INSERT INTO Sub_category (size) VALUES('$_POST[".$categories"]');";
-$sql3 = "INSERT INTO Pages (Description) VALUES('$_POST[".$description"]');";
 
 
-if (mysqli_query($conn, $sql)) {
+//SQL Statements
+$sql = "INSERT INTO Space (space_name,resv_start_date,resv_end_date,price,service) VALUES('$space_name','$availability_from','$availability_till','$PricePerHour','$Amenities');";
+$sql1 = "INSERT INTO Space_addr (street,room,state,zip_code,) VALUES('$street','$apt','$state','$zipcode');";
+$sql2 = "INSERT INTO Sub_category (size) VALUES('$categories');";
+$sql3 = "INSERT INTO Pages (Description) VALUES('$description');";
+
+
+//Connecting SQL Query
+//Checking if all required feilds are not filled
+if(empty($space_name) || empty($street) || empty($apt) || empty($state) || empty($zipcode) || empty($availability_from) || empty($availability_till) || empty($categories)|| empty($PricePerHour)|| empty($description)|| empty($Amenities))
+{
+    echo "You did not fill out the required fields.";
+}
+elseif (mysqli_query($conn, $sql)) {
   echo "Congratulations, New Space created successfully";
 } else {
   echo "Error: " .$sql ."<br>" .mysqli_error($conn);
