260b596bdfa094905bfbd6745fc5d64bbb1662ce
iamnsrivastava <35356736+iamnsrivastava@users.noreply.github.com>
2018-04-10 20:29:58 -0400

    Add files via upload

--- Phase3/Views/spaces_page.php
+++ Phase3/Views/spaces_page.php
@@ -3,21 +3,21 @@
 echo "Welcome to the Space Page <br>";
 $space_name = $_POST[space_name];
 $street = $_POST["street"];
-$apt = $_POST[apt];
+$apt = $_POST["apt"];
 $state = $_POST["state"];
-$zipcode = $_POST[zipcode];
-$availability_from = $_POST[];
-$availability_till = $_POST[];
-$categories = $_POST[];
-$PricePerHour = $POST[price];
+$zipcode = $_POST["zipcode"];
+$availability_from = $_POST["from"];
+$availability_till = $_POST["till"];
+$categories = $_POST["Categories"];
+$PricePerHour = $POST["price"];
 $description = $POST["description"];
-$Amenities = $POST[];
-$Pictures = $POST[pic];
+$Amenities = $POST["amenities"];
+// Need to add $Pictures = $POST["pic"];
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Data_bus_V2.0"
+$dbname = "database_phase3"
 
 //Creating Connection
 $conn = mysqli_connect($servername,$username,$password,$dbname);
@@ -25,8 +25,12 @@ $conn = mysqli_connect($servername,$username,$password,$dbname);
 if (!$conn) {
   die("Connection Failed: " .mysqli_connect_error());
 }
+//SQL Statements 
+$sql = "INSERT INTO Space (space_name,resv_start_date,resv_end_date,price,service) VALUES('$_POST["space_name"]','$_POST["from"]','$_POST["till"]'','$_POST["price"]','$_POST[amenities]');";
+$sql1 = "INSERT INTO Space_addr (street,room,state,zip_code,) VALUES('$_POST[".$street"]','$_POST[".$apt"]','$_POST[".$state"]'','$_POST[".$zipcode"]');";
+$sql2 = "INSERT INTO Sub_category (size) VALUES('$_POST[".$categories"]');";
+$sql3 = "INSERT INTO Pages (Description) VALUES('$_POST[".$description"]');";
 
-$sql = "INSERT INTO '$Space_name' (space_name,street,apt,state,zipcode,availability_from,availability_till,categories,price,description,amenities,pic) VALUES('".$space_name"','".$street"'','.$apt','".$state"'','.$zipcode','.$availability_from','.$availability_till','".$categories",'.$price','".$description"'','".$amenities"'','.$pic')
 
 if (mysqli_query($conn, $sql)) {
   echo "Congratulations, New Space created successfully";
